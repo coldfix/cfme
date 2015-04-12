d74add0693e332eec80a4d8bea0a67d4d468e6dd
Thomas Gläßle <t_glaessle@gmx.de>
2015-04-12 03:32:03 +0200

    Ensure stack unwinding in case of exception

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include "cfme.h"
 
 int main(int argc, char** argv, char** env)
+try
 {
     size_t width = 2;
     if (argc >= 2)
         width = std::atol(argv[1]);
     solve(width);
     return 0;
+}
+catch (...)
+{
+    throw;
 }
