2bd723b8b644bb7ceca6f21895cdf60f8479ae48
Thomas Gläßle <t_glaessle@gmx.de>
2015-05-12 16:41:21 +0200

    Fix bug in generate_git_info.sh

--- generate_git_info.sh
+++ generate_git_info.sh
@@ -4,9 +4,9 @@ commit=$(git rev-parse HEAD)
 status=$(git status --untracked-files=no --porcelain)
 
 if [[ -z $status ]]; then
-    dirty=true
-else
     dirty=false
+else
+    dirty=true
 fi
 
 cat <<EOF
